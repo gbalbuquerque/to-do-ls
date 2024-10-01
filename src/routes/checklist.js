a2c5d7716750dfaa82d5f3f9be842ba057bceed6
Guilherme <guilherme@applayos.com>
2024-10-01 14:48:51 -0300

    post

---
+++ src/routes/checklist.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req,res) => {
+  console.log("Olá");
+  res.send();
+})
+
+router.post('/', (req,res) => {
+  console.log(req.body);
+  res.status(200).send(req.body)
+})
+
+router.get('/:id')
+
+module.exports = router
