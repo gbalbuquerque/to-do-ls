ca751619914f388a0f6470fec09087e5843caf49
Guilherme <guilherme@applayos.com>
2024-10-01 11:59:42 -0300

    Rota e Json

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-const express = require('express')
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) => {
+  res.send('<h1> Minha Lista de Tarefas :) <h1>');
+})
+
+app.get('/json',(req,res) => {
+  res.json(
+    {
+      title: "Tarefa X",
+      done: true,
+      
+    }
+  )
+})
+
+app.listen(3000, () => {
+  console.log('O servidor foi iniciado');
+})
