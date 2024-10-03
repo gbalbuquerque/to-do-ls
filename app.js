48061f8de3298c9b88d9d4d66a31e7937e3610f4
Guilherme <guilherme@applayos.com>
2024-10-03 10:06:55 -0300

    config ejs pt1

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const path = require('path')
+
 const checkListRouter = require('./src/routes/checklist')
+
 require('./config/database')
 
 const app = express();
 app.use(express.json());
 
+app.set('views', path.join(__dirname,'src/views'));
+app.set('view engine' , 'ejs');
+
 app.use('/checklists', checkListRouter);
 
-app.listen(3001, () => {
+
+app.listen(3000, () => {
   console.log('O servidor foi iniciado');
 })
