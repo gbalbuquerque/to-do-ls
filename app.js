68b246b4984321398369f9dbbe72a1ea354c44d3
Guilherme <guilherme@applayos.com>
2024-10-10 13:33:32 -0300

    nem eu sei oq mudo

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ const checkListRouter = require('./src/routes/checklist');
 const rootRouter = require('./src/routes/index');
 const methodOverride = require('method-override');
 
+
 require('./config/database');
+
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method', { methods: ['POST', 'GET']}));
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('views', path.join(__dirname,'src/views'));
-app.set('view engine' , 'ejs');
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
 
-app.use('/' , rootRouter);
+app.use('/', rootRouter);
 app.use('/checklists', checkListRouter);
 
 app.listen(3000, () => {
-  console.log('O servidor foi iniciado');
+  console.log('Servidor foi iniciado');
 })
